d7c2de9ca8421488c99c2d9b2807dad21003e312
Adam Ryman <adamr@tune.com>
2017-03-07 22:37:34 -0800

    Add flags

--- main.go
+++ main.go
@@ -8,13 +8,21 @@ import (
 	"github.com/adamryman/radialwave/parse"
 	"github.com/adamryman/radialwave/viz"
 	"github.com/mjibson/go-dsp/wav"
+	flag "github.com/spf13/pflag"
+)
+
+var (
+	r    = flag.IntP("radius", "r", 3000, "radius")
+	file = flag.StringP("outfile", "o", "output.png", "file output")
+	fill = flag.Float64P("fill", "f", 1, "fill between sections")
 )
 
 func main() {
-	if len(os.Args) < 2 {
+	flag.Parse()
+	if len(flag.Args()) < 1 {
 		fmt.Println("Need a second argument")
 	}
-	in, err := os.Open(os.Args[1])
+	in, err := os.Open(flag.Arg(0))
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -31,13 +39,13 @@ func main() {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-	f, err := os.Create("output.png")
+	f, err := os.Create(*file)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
 
-	circle, err := viz.Circle(freq)
+	circle, err := viz.Circle(freq, *r, *fill)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
