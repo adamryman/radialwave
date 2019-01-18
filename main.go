2c29f6d2b2491861fa886d3498a83885a428df9b
Adam Ryman <adamryman@gmail.com>
2019-01-17 22:23:17 -0800

    Restructure main for easier future testing

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"image"
 	"image/png"
+	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -26,8 +27,7 @@ var (
 	// TODO: Input validation
 	bpm = flag.Float64P("bpm", "b", 60, "bpm of input, one arc of the circle for every beat in the input.")
 
-	// TODO: Implement properly
-	simple = flag.Int("simple", 0, "produce simple circle with passed number of chords")
+	chirp = flag.Int("chirp", 0, "produces a circle using a chrirp with passed number of frequencies/chords")
 
 	animate = flag.StringP("animate", "a", "", "output pngs to be animated, will put frames into directory passed into flag")
 	ffmpeg  = flag.Bool("ffmpeg", false, "run ffmpeg for creating animation with music")
@@ -35,158 +35,165 @@ var (
 	outFile = flag.StringP("outfile", "o", "output.png", "file output")
 )
 
-var inputFile string
-
 func main() {
-	os.Exit(run())
+	err := run()
+	if err != nil {
+		log.Fatalln(err)
+	}
 }
 
-func run() int {
+func run() error {
 	flag.Parse()
-	var errMessage error
-	defer func() {
-		if errMessage != nil {
-			fmt.Fprintln(os.Stderr, errMessage)
+
+	var inputFile string
+	var err error
+
+	freq := createChirp(*chirp)
+
+	// If we don't have frequencies at this point, chirp was not used
+	if len(freq) == 0 {
+		if len(flag.Args()) < 1 {
+			return errors.New("need input file")
 		}
-	}()
+		inputFile = flag.Arg(0)
 
-	var renderFunc func([]int) error
-	if *animate == "" {
-		renderFunc = renderPNG
-	} else {
-		renderFunc = renderPNGs
-	}
-
-	if *simple != 0 {
-		freq := make([]int, *simple)
-		for i := 0; i < len(freq); i++ {
-			freq[i] = i
-		}
-
-		err := renderFunc(freq)
+		log.Printf("Extracting frequency content of input file %s\n", inputFile)
+		freq, err = getMaxFrequenciesFromWav(inputFile)
 		if err != nil {
-			errMessage = errors.Wrap(err, "error creating simple circle")
-			return 1
+			return err
 		}
-		return 0
 	}
+	animatePath := *animate
 
-	if len(flag.Args()) < 1 {
-		errMessage = errors.New("need input file")
-		return 1
-	}
-	inputFile = flag.Arg(0)
-
-	err := handleInputWav(inputFile, renderFunc)
-	if err != nil {
-		errMessage = err
-		return 1
-	}
-
-	return 0
-}
-
-func renderPNGs(freq []int) error {
-	circles, err := viz.Circles(freq, *radius, *fill)
-	if err != nil {
-		return err
-	}
-
-	var wg sync.WaitGroup
-
-	err = os.MkdirAll(*animate, 0777)
-	if err != nil {
-		return err
-	}
-	type indexCircle struct {
-		Index  int
-		Circle image.Image
-	}
-	circleChan := make(chan indexCircle)
-	for i := 0; i < runtime.NumCPU(); i++ {
-		wg.Add(1)
-		go func() {
-			defer wg.Done()
-			for c := range circleChan {
-				f, err := os.Create(filepath.Join(*animate, strconv.Itoa(c.Index)+".png"))
-				if err != nil {
-					fmt.Fprintln(os.Stderr, err)
-				}
-
-				err = png.Encode(f, c.Circle)
-				f.Close()
-				if err != nil {
-					fmt.Fprintln(os.Stderr, err)
-				}
-			}
-		}()
-	}
-	for i, c := range circles {
-		circleChan <- indexCircle{i, c}
-	}
-	close(circleChan)
-
-	wg.Wait()
-
-	bps := *bpm / 60.0
-	// TODO: ffmpeg -i harder.wav -r 2.053333 -i ./harder_highdef/%d.png -c:v libx264 -b:v 1M -f mp4 -preset fast -pix_fmt yuv420p -r 2.053333 -acodec aac harder_highdef.mp4
-	// TODO: make mp4
-	fmt.Printf("ffmpeg -i %s -r %f -i ./%s/%%d.png -c:v libvpx -b:v 1M -crf 4 %s\n", inputFile, bps, *animate, *outFile)
-	if *ffmpeg {
-		// TODO: ffmpeg -i harder.wav -r 2.053333 -i ./harder_highdef/%d.png -c:v libx264 -b:v 1M -f mp4 -preset fast -pix_fmt yuv420p -r 2.053333 -acodec aac harder_highdef.mp4
-		// TODO: make mp4
-		ffmpegCmd := exec.Command("ffmpeg", strings.Split(fmt.Sprintf("-i %s -r %f -i ./%s/%%d.png -c:v libvpx -b:v 1M -crf 4 %s", inputFile, bps, *animate, *outFile), " ")...)
-		ffmpegCmd.Stdin = os.Stdin
-		ffmpegCmd.Stderr = os.Stderr
-		ffmpegCmd.Stdout = os.Stdout
-		err = ffmpegCmd.Run()
+	if animatePath != "" {
+		circles, err := viz.Circles(freq, *radius, *fill)
+		if err != nil {
+			return err
+		}
+		err = renderPNGs(circles, animatePath)
 		if err != nil {
 			return err
 		}
 
+		if *ffmpeg {
+			bps := *bpm / 60.0
+			// TODO: ffmpeg -i harder.wav -r 2.053333 -i ./harder_highdef/%d.png -c:v libx264 -b:v 1M -f mp4 -preset fast -pix_fmt yuv420p -r 2.053333 -acodec aac harder_highdef.mp4
+			// TODO: make mp4
+			log.Printf("ffmpeg -i %s -r %f -i ./%s/%%d.png -c:v libvpx -b:v 1M -crf 4 %s\n", inputFile, bps, animatePath, *outFile)
+			// TODO: ffmpeg -i harder.wav -r 2.053333 -i ./harder_highdef/%d.png -c:v libx264 -b:v 1M -f mp4 -preset fast -pix_fmt yuv420p -r 2.053333 -acodec aac harder_highdef.mp4
+			// TODO: make mp4
+			ffmpegCmd := exec.Command("ffmpeg", strings.Split(fmt.Sprintf("-i %s -r %f -i ./%s/%%d.png -c:v libvpx -b:v 1M -crf 4 %s", inputFile, bps, animatePath, *outFile), " ")...)
+			ffmpegCmd.Stdin = os.Stdin
+			ffmpegCmd.Stderr = os.Stderr
+			ffmpegCmd.Stdout = os.Stdout
+			err = ffmpegCmd.Run()
+			if err != nil {
+				return err
+			}
+
+		}
+
+		return nil
 	}
 
-	return nil
-
-}
-
-func renderPNG(freq []int) error {
-	// TODO: Break this up so I can create a simple cirle with the number of chords passed
 	circle, err := viz.Circle(freq, *radius, *fill)
 	if err != nil {
 		return err
 	}
+	err = renderPNG(circle)
+	if err != nil {
+		return err
+	}
 
+	return nil
+}
+
+func createChirp(chirpCount int) []int {
+	freq := make([]int, chirpCount)
+	for i := 0; i < len(freq); i++ {
+		freq[i] = i
+	}
+	return freq
+}
+
+func getMaxFrequenciesFromWav(inputFile string) ([]int, error) {
+	in, err := os.Open(inputFile)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot open file %s", inputFile)
+	}
+	w, err := wav.New(in)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot open %s as wav file", inputFile)
+	}
+	freq, err := parse.WavIntoMaxAmplitudeFrequencies(w, *bpm)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot parse %s as wav file", inputFile)
+	}
+	return freq, nil
+}
+
+// renderPNGs takes a slice of images, and renders them into the directory path
+func renderPNGs(pngs []image.Image, path string) error {
+
+	var wg sync.WaitGroup
+
+	// create directory
+	err := os.MkdirAll(path, 0777)
+	if err != nil {
+		return err
+	}
+
+	// create indexable pngs for file names
+	type indexedPNG struct {
+		Index int
+		PNG   image.Image
+	}
+
+	// create channel for work to be added to
+	pngChan := make(chan indexedPNG)
+	// put images into channel to be processed
+	go func() {
+		for i, p := range pngs {
+			log.Printf("Rending frame %d / %d\n", i, len(pngs))
+			pngChan <- indexedPNG{i, p}
+		}
+		close(pngChan)
+	}()
+
+	// Create workers
+	for i := 0; i < runtime.NumCPU(); i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			for p := range pngChan {
+				f, err := os.Create(filepath.Join(path, strconv.Itoa(p.Index)+".png"))
+				if err != nil {
+					log.Println(err)
+				}
+
+				err = png.Encode(f, p.PNG)
+				f.Close()
+				if err != nil {
+					log.Println(err)
+				}
+			}
+		}()
+	}
+
+	// Wait for workers to finish
+	wg.Wait()
+
+	return nil
+}
+
+func renderPNG(circle image.Image) error {
 	f, err := os.Create(*outFile)
 	if err != nil {
 		return err
 	}
 
-	//freq := []int{0, 100, 1000, 100000}
-
 	err = png.Encode(f, circle)
 	f.Close()
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func handleInputWav(input string, renderFunc func([]int) error) error {
-	in, err := os.Open(input)
-	if err != nil {
-		return errors.Wrapf(err, "cannot open file %s", input)
-	}
-	w, err := wav.New(in)
-	if err != nil {
-		return errors.Wrapf(err, "cannot open %s as wav file", input)
-	}
-	freq, err := parse.WavIntoMaxAmplitudeFrequencies(w, *bpm)
-	if err != nil {
-		return errors.Wrapf(err, "cannot parse %s as wav file", input)
-	}
-	err = renderFunc(freq)
 	if err != nil {
 		return err
 	}
